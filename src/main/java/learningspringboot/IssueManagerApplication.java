1984b59df361dca4405008ac8a93a80c92ba12e7
zhenkyle <zhenkyle@gmail.com>
2016-01-18 20:15:22 +0800

    Initial commit

---
+++ src/main/java/learningspringboot/IssueManagerApplication.java
@@ -0,0 +1,12 @@
+package learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueManagerApplication.class, args);
+	}
+}
